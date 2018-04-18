1ba407fc4f5035f672ae3f680fbf2a8042663730
Leyla Samiee <lsamie@gmail.com>
2018-04-18 01:12:02 -0400

    Update ReadData.java

--- ReadData.java
+++ ReadData.java
@@ -55,16 +55,25 @@ try {
   // yPosition of the trap = trapPositions[1][trapCounter]
   public int [][] trapPositions = new int[2][mapSide*mapSide];
   public int trapCounter = -1;
-  public boolean solveMaze (
-  //assuming all maps are square
-  //' ' is where the monster can move
-  // '+', '-'. '|' are obstacles
-  //'N' is start position
-  //'G' is goal
-  //'P' is potential trap location
-  //x is column, y is row
-  //mark the path in the maze via symbol 'S'
-  //if x = < 0, or y < 0 or x or y > mapSide (out of bounds) return false
+  
+ // method Solve
+ solveMaze(
+        
+ } catch (Exception e) {
+    System.out.println("Exception occurred");
+ } // catch 
+} // main 
+public boolean solveMaze (char[][] mapChar) 
+{
+    //assuming all maps are square
+    //' ' is where the monster can move
+    // '+', '-'. '|' are obstacles
+    //'N' is start position
+    //'G' is goal
+    //'P' is potential trap location
+    //x is column, y is row
+    //mark the path in the maze via symbol 'S'
+    //if x = < 0, or y < 0 or x or y > mapSide (out of bounds) return false
     if ((x < 0) || (y < 0) || (x > mapSide) || (y > mapSide) {
       return false;
     }
@@ -74,22 +83,16 @@ try {
     //if value of mapText[x][y] == '+' || '-' || '|' (obstacle) return false
     //if value of mapText[x][y] == 'G' return true got to the goal
     if (mapText[x][y] == 'G') {
-   return true;
+      return true;
+    }
+    //once conditions are fullfiled, mapText[x][y] = 'S' any Potential Trap is considered a possible path
+    if (mapText[x][y] == 'P') {
+      trapCounter++; 
+      trapPositions[0][trapCounter] = x;
+      trapPositions[1][trapCounter] = y;
+    }
+    //if any directional cell returns true, method returns true
+    //for each direction which returns false, unmark 'S';  mapText[x][y] = 'Z'
+    //else return false
   }
-  //once conditions are fullfiled, mapText[x][y] = 'S' any Potential Trap is considered a possible path
-  if (mapText[x][y] == 'P') {
-    trapCounter++; 
-    trapPositions[0][trapCounter] = x;
-    trapPositions[1][trapCounter] = y;
-  }
-  //if any directional cell returns true, method returns true
-  //for each direction which returns false, unmark 'S';  mapText[x][y] = 'Z'
-  //else return false
-
-  
-  }
-      } catch (Exception e) {
-        System.out.println("Exception occurred");
-      }
- }
-}
+} // Class
